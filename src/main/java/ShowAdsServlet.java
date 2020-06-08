97314d4323eab64eacf090e2d94bae18c0bb448d
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-08 15:51:50 -0500

    create ShowAdsServlet making index available hwne user visits /ads

---
+++ src/main/java/ShowAdsServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ShowAdsServlet", urlPatterns = "/ads")
+public class ShowAdsServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //use the factory to get the dao object
+        Ads adsDao = DaoFactory.getAdsDao();
+
+        //use a method on the dao to get all ads
+        List<Ad> ads = adsDao.all();
+
+        //pass the data to the jsp
+        request.setAttribute("ads", ads);
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
+
+    }
+}
