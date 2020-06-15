7b03529bfa95e19a897aeb5c999b3235500934ed
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-06-14 21:53:09 -0500

    initial commit from my own practice template

---
+++ src/main/java/com.codeup.adlister/controllers/CreateAdServlet.java
@@ -0,0 +1,37 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
+public class CreateAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //if user is not logged in, make them log in to create an ad:
+        if( request.getSession().getAttribute("user") == null ) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+            .forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        Ad ad = new Ad(
+            user.getId(), // rather than hard-coding the userId to 1, when a logged in user creates an ad, assign the userId of that user to the ad
+//            user.getUsername(),
+            request.getParameter("title"),
+            request.getParameter("description")
+        );
+        DaoFactory.getAdsDao().insert(ad);
+        response.sendRedirect("/ads");
+    }
+}
